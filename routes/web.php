7e1b12059f215aa97b1fb2ac6e3709619a3d0e4b
zhangfan <cyouho@hotmail.co.jp>
2022-04-03 09:30:16 +0800

    添加登录和注册页面

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [AuthController::class, 'showRegisterPage']);
+Route::get('/login', [AuthController::class, 'showLoginPage']);
