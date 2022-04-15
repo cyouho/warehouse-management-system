4faeffd34d883459197eaab6513ca0072a9d59d3
zhangfan <cyouho@hotmail.co.jp>
2022-04-15 10:59:00 +0800

    设计总体view排版

--- public/js/add_goods.js
+++ public/js/add_goods.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
+    $("#purchase-date").datepicker({
+        language: "zh-CN",
+        autoclose: true,
+        clearBtn: true,
+        todayBtn: "linked",
+        todayHighlight: true,
+    });
 
+    $("#expiry-date").datepicker({
+        language: "zh-CN",
+        autoclose: true,
+        clearBtn: true,
+        todayBtn: "linked",
+        todayHighlight: true,
+    });
 });
--- resources/views/index/index_layer.blade.php
+++ resources/views/index/index_layer.blade.php
@@ -2,6 +2,10 @@
 
 <head>
     @include('global.global_header')
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-datepicker.css">
+
+    <script type="text/javascript" src="/js/bootstrap-datepicker.min.js" charset="utf-8"></script>
+    <script type="text/javascript" src="/js/bootstrap-datepicker.zh-CN.min.js" charset="utf-8"></script>
 </head>
 
 <body>
