ff4d362da072824b1fe6088183b7c2da1aaa0bde
zhangfan <cyouho@hotmail.co.jp>
2022-04-15 09:17:54 +0800

    添加药品数据库迁移文件

---
+++ database/migrations/2022_04_15_091336_medicines.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Medicines extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicines', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id()->unique();
+            $table->integer('user_id');
+            $table->integer('medicine_level'); // 药品临期等级
+            $table->string('medicine_name'); // 药品官方名称
+            $table->string('medicine_sub_name'); // 药品自定义名称
+            $table->integer('amount'); // 数量
+            $table->integer('shelves'); // 货架号
+            $table->integer('number_of_plies'); // 货架上第几层
+            $table->timestamp('purchase_date'); // 采购时间
+            $table->timestamp('expiry_date'); // 过期时间
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicines');
+    }
+}
