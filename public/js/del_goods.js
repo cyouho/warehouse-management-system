a832de740bea22c07e553641d2c958dee060926b
zhangfan <cyouho@hotmail.co.jp>
2022-04-28 16:38:09 +0800

    临过期显示添加删除物品功能

--- public/js/del_goods.js
+++ public/js/del_goods.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
     var token = $('meta[name="csrf-token"]').attr('content');
 
+    $(".del-goods-for-expiry").click(function () {
+        goodsId = $(this).attr("goods-id-for-expiry");
+        table = $(this).attr("table-for-expiry");
+        $(this).closest("tr").remove();
+
+        delGoods(goodsId, table);
+    });
+
     $("#search-goods-result").on("click", ".del-goods-for-search", function () {
         goodsId = $(this).attr("goods-id");
         table = $(this).attr("table");
